f0efe3be721654abbc38ec5b19a9c22f87572e37
Stefan <1stefan.wolf1@gmail.com>
2026-01-11 10:59:40 +0100

    router and pages added for future sites

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/pages/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: Home,
     },
     {
       path: '/about',
@@ -15,8 +15,33 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('@/pages/AboutMe.vue'),
     },
+    {
+      path: '/cv',
+      name: 'cv',
+      component: () => import('@/pages/Cv.vue'),
+    },
+    {
+      path: '/aboutme',
+      name: 'aboutme',
+      component: () => import('@/pages/AboutMe.vue'),
+    },
+    {
+      path: '/goals',
+      name: 'goals',
+      component: () => import('@/pages/Goals.vue'),
+    },
+    {
+      path: '/references',
+      name: 'references',
+      component: () => import('@/pages/References.vue'),
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('@/pages/Projects.vue'),
+    }
   ],
 })
 
