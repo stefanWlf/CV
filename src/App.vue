2c3ca3f59368fc75aa042d55d1867b8f9f8ca718
Stefan <1stefan.wolf1@gmail.com>
2026-01-23 23:04:30 +0100

    Feat: Added my own background, with an subtle animation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,63 @@
 <template>
- <RouterView />
+  <!-- Decorative circle Elements -->
+  <div class="circle circle-purple"></div>
+  <div class="circle circle-yellow"></div>
+  <div class="circle circle-pink"></div>
+  
+  <RouterView/>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 </script>
+
+<style scoped>
+.circle {
+  position: fixed;
+  width: 18rem;
+  height: 18rem;
+  border-radius: 9999px;
+  mix-blend-mode: multiply;
+  filter: blur(4rem);
+  opacity: 0.2;
+}
+
+.circle-purple {
+  top: 5rem;
+  left: 2.5rem;
+  background-color: #d8b4fe;
+  animation: circle 7s infinite;
+}
+
+.circle-yellow {
+  top: 10rem;
+  right: 2.5rem;
+  background-color: #fde047;
+  animation: circle 7s infinite 2s;
+}
+
+.circle-pink {
+  bottom: 5rem;
+  left: 50%;
+  background-color: #fbcfe8;
+  animation: circle 7s infinite 4s;
+}
+
+@media (prefers-color-scheme: dark) {
+  .circle {
+    mix-blend-mode: soft-light;
+  }
+
+  .circle-purple {
+    background-color: var(--purple-600);
+  }
+  
+  .circle-yellow {
+    background-color: #713f12;
+  }
+  
+  .circle-pink {
+    background-color: #831843;
+  }
+}
+</style>
